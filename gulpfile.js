e284221ccfe6d7b660e30b9a4febb14a1cdf4bb6
sifiso khumalo <acoussounds@gmail.com>
2020-05-20 06:30:39 +0200

    awesome changes

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+const Gulp = require('gulp');
+
+// TOP lEVEL functions
+// gulp.task - /define Tasks
+// gulp.src - point to files to use
+// gulp.dest - Points to folder to output
+// gulp.watch - watch files and folders for changes
+
+// Lags Message
+Gulp.task('message', function(){
+    return console.log('Gulp is running...');
+})
+
+
+ 
